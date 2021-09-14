c8f10cb9b088508440e3fd2754002ca98d89ea7c
Mehmet Aziz Algüllü <maacode@hotmail.com>
2021-09-14 12:38:18 +0300

    Copied html and css content from link of https://codepen.io/mehmetseven/pen/OJRzLjV?editors=1010 and pasted into todo-app project folder. Added many component as needed.

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 
+import Header from './components/Header'
+import Content from './components/Content'
+import Footer from './components/Footer'
+
 function App() {
   return (
-    <div className="App">
+    <>
+      <section className="todoapp">
+        <Header />
+        <Content />
+      </section>
 
-    </div>
+      <Footer />
+    </>
   );
 }
 
---
+++ todo-app/src/components/Form.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Form() {
+    return (
+        <form>
+            <input className="new-todo" placeholder="What needs to be done?" autofocus />
+        </form>
+    )
+}
+
+export default Form
