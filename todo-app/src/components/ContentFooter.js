8b99c3efdce4ddc2d6baaa6e67cc0556aee734b0
Mehmet Aziz Algüllü <maacode@hotmail.com>
2021-09-15 23:41:01 +0300

    This component has been made dynamic from static via related state datas.

--- todo-app/src/components/ContentFooter.js
+++ todo-app/src/components/ContentFooter.js
@@ -1,27 +1,55 @@
 import React from 'react'
 
+import { useSelector, useDispatch } from 'react-redux'
+import { changeActiveFilter, clearCompleted, selectTodos } from '../redux/todos/todosSlice'
+
 function ContentFooter() {
+    const dispatch = useDispatch();
+
+    const items = useSelector(selectTodos);
+    const itemsLeft = items.filter((item) => !item.completed).length;
+    
+    const activeFilter = useSelector(state => state.todos.activeFilter);
+
+    // const allItem = items.length;
+
     return (
         <footer className="footer">
             <span className="todo-count">
-                <strong>2</strong> {" "}
-                items left
+                <strong>{itemsLeft}</strong> {" "}
+                item{itemsLeft > 1 && 's'} left
             </span>
 
             <ul className="filters">
                 <li>
-                    <a href="/#" className="selected">All</a>
+                    <a
+                        href="/#"
+                        className={activeFilter === 'all' ? 'selected' : ''}
+                        onClick={() => dispatch(changeActiveFilter('all'))}
+                    >
+                        All
+                    </a>
                 </li>
                 <li>
-                    <a href="/#">Active</a>
+                    <a
+                        href="/#"
+                        className={activeFilter === 'active' ? 'selected' : ''}
+                        onClick={() => dispatch(changeActiveFilter('active'))}
+                    >
+                        Active</a>
                 </li>
                 <li>
-                    <a href="/#">Completed</a>
+                    <a
+                        href="/#"
+                        className={activeFilter === 'completed' ? 'selected' : ''}
+                        onClick={() => dispatch(changeActiveFilter('completed'))}
+                    >
+                        Completed</a>
                 </li>
             </ul>
 
-            <button className="clear-completed">
-                Clear completed
+            <button className="clear-completed" onClick={() => dispatch(clearCompleted())}>
+                Clear    completed
             </button>
         </footer>
     )
