a60640ca5b5fe499a27b805c41fe2f16e443b68c
Mehmet Aziz Algüllü <maacode@hotmail.com>
2021-09-15 12:55:44 +0300

    Added toggle reducer. Item.completed will change when clicked checkbox field

--- todo-app/src/redux/todos/todosSlice.js
+++ todo-app/src/redux/todos/todosSlice.js
@@ -20,9 +20,17 @@ export const todosSlice = createSlice({
         addTodo: (state, action) => {
             //datayı klonlamadan iş yapma! burada react-toolkit arka planda klonlayıp da ekliyor.
             state.items.push(action.payload); //gönderilen actionun içeriğini pushla.
-        }
+        },
+        //toggle : aktifse deaktif et, deaktifse aktif et demekmiş.
+        toggle: (state, action) => {
+            const { id } = action.payload; //bize action ile id gönderilecek.
+
+            const item = state.items.find((item) => item.id === id);
+
+            item.completed = !item.completed;
+        },
     },
 });
 
-export const { addTodo } = todosSlice.actions;
+export const { addTodo, toggle } = todosSlice.actions;
 export default todosSlice.reducer; //dışarıdan isterlerse xxxx olarak alsınlar, ben bunu gönderiyorum.
