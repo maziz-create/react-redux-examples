759e81262a342018429dc92773ef882e24ef6898
Mehmet Aziz Algüllü <maacode@hotmail.com>
2021-09-15 12:58:56 +0300

    Imported useDispatch for dispatching toggle. When checkbox is clicked, TodoList Component will dispatch toggle function to store.

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 //bir event gönderecek olursak slice'ı da import etmeliyiz.
+import { toggle } from '../redux/todos/todosSlice'
 
 function TodoList() {
     const items = useSelector(state => state.todos.items);
+
+    const dispatch = useDispatch(); //store'a bir şey göndermek istediğimizde kullanmak zorundayız.
 
     console.log(items);
 
@@ -14,7 +17,12 @@ function TodoList() {
                 items.map((item) => (
                     <li key={item.id} className={item.completed ? 'completed' : ''}>
                         <div className="view">
-                            <input className="toggle" type="checkbox" />
+                            <input
+                                className="toggle"
+                                type="checkbox"
+                                checked={item.completed}
+                                onChange={() => dispatch(toggle({ id: item.id }))}
+                            />
                             <label>{item.title}</label>
                             <button className="destroy"></button>
                         </div>
