5fefb2d861fa8a33baa4a59297f8f4038c4ee6e1
Mehmet Aziz Algüllü <maacode@hotmail.com>
2021-09-15 12:36:13 +0300

    Sent todo from Input via addTodo dispatch to Store.

--- todo-app/src/components/Form.js
+++ todo-app/src/components/Form.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import { useState } from 'react'
+
+import { useDispatch } from 'react-redux'
+import { nanoid } from '@reduxjs/toolkit'
+import { addTodo } from '../redux/todos/todosSlice'
 
 function Form() {
+    const [title, setTitle] = useState('');
+
+    const dispatch = useDispatch(); //action göndereceğiz.
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); //sayfa yenilenmemesi için
+
+        setTitle('');
+
+        dispatch(
+            addTodo(
+                {
+                    id: nanoid(), //random id üretiyor.
+                    title, //title: title kendisi yapıyor. (ikinci title => state'imiz)
+                    completed: false,
+                }
+            )
+        );
+
+        console.log('test');
+    }
+
     return (
-        <form>
-            <input className="new-todo" placeholder="What needs to be done?" autoFocus />
+        <form onSubmit={handleSubmit}>
+            <input
+                className="new-todo"
+                placeholder="What needs to be done?"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                autoFocus />
         </form>
     )
 }
