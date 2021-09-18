e27b62d0099c8e0a41043ff0bb1a7f1b2a57e5c6
Mehmet Aziz Algüllü <maacode@hotmail.com>
2021-09-18 13:35:13 +0300

    SearchInput Component moved to its folder/index.js

---
+++ homework-1/src/components/SearchInput/index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+import { Input, Text } from '@chakra-ui/react'
+
+function SearchInput() {
+    return (
+        <div>
+            <Text
+                fontSize="22px"
+                textAlign="center"
+                fontWeight="bold"
+                color="#808080"
+                marginTop="3"
+                marginBottom="3"
+            >
+                NotesApp</Text>
+
+            <Input
+                backgroundColor="#fff"
+                placeholder="Search..."
+                marginLeft="100"
+                marginBottom="3"
+                borderRadius="3xl"
+                width="60%"
+
+                focusBorderColor="red.200"
+            />
+        </div>
+    )
+}
+
+export default SearchInput
