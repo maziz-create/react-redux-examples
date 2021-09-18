fc9ab7c2de5b3b27b356a35646e3724a549b9c46
Mehmet Aziz Algüllü <maacode@hotmail.com>
2021-09-18 14:12:22 +0300

    Changed bg color, Flex and Box is used for aligning

--- homework-1/src/components/Palette/index.js
+++ homework-1/src/components/Palette/index.js
@@ -3,66 +3,67 @@ import React from 'react'
 // import styles from './styles.module.css'
 import './style.css'
 
-import { Button, Flex } from '@chakra-ui/react'
+import { Button, Flex, Box } from '@chakra-ui/react'
 
 function Palette() {
     return (
-        <Flex>
-            <label className="checkboxContainer">
-                <input
-                    type="checkbox"
-                    className="checkbox checkbox-pink"
-                />
-                <span className="checkmark checkmark-pink"></span>
-            </label>
+        <Flex justify="space-between" style={{ marginTop: "5px" }} >
+            <Flex >
+                <label className="checkboxContainer">
+                    <input
+                        type="checkbox"
+                        className="checkbox checkbox-pink"
+                    />
+                    <span className="checkmark checkmark-pink"></span>
+                </label>
 
-            <label className="checkboxContainer">
-                <input
-                    type="checkbox"
-                    className="checkbox checkbox-purple"
-                />
-                <span className="checkmark checkmark-purple"></span>
-            </label>
+                <label className="checkboxContainer">
+                    <input
+                        type="checkbox"
+                        className="checkbox checkbox-purple"
+                    />
+                    <span className="checkmark checkmark-purple"></span>
+                </label>
 
-            <label className="checkboxContainer">
-                <input
-                    type="checkbox"
-                    className="checkbox checkbox-yellow"
-                />
-                <span className="checkmark checkmark-yellow"></span>
-            </label>
+                <label className="checkboxContainer">
+                    <input
+                        type="checkbox"
+                        className="checkbox checkbox-yellow"
+                    />
+                    <span className="checkmark checkmark-yellow"></span>
+                </label>
 
-            <label className="checkboxContainer">
-                <input
-                    type="checkbox"
-                    className="checkbox checkbox-blue"
-                />
-                <span className="checkmark checkmark-blue"></span>
-            </label>
+                <label className="checkboxContainer">
+                    <input
+                        type="checkbox"
+                        className="checkbox checkbox-blue"
+                    />
+                    <span className="checkmark checkmark-blue"></span>
+                </label>
 
-            <label className="checkboxContainer">
-                <input
-                    type="checkbox"
-                    className="checkbox checkbox-green"
-                />
-                <span className="checkmark checkmark-green"></span>
-            </label>
+                <label className="checkboxContainer">
+                    <input
+                        type="checkbox"
+                        className="checkbox checkbox-green"
+                    />
+                    <span className="checkmark checkmark-green"></span>
+                </label>
+            </Flex>
 
+            <Box>
+                <Button
+                    paddingLeft="35"
+                    paddingRight="35"
+                    height="27"
+                    marginLeft="50"
 
-
-
-            <Button
-                paddingLeft="35"
-                paddingRight="35"
-                height="30"
-                marginLeft="50"
-
-                colorScheme="pink"
-                borderRadius="3xl"
-            >
-                Add
-            </Button>
-        </Flex>
+                    colorScheme="red"
+                    borderRadius="3xl"
+                >
+                    Add
+                </Button>
+            </Box>
+        </ Flex>
     )
 }
 
