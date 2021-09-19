b9d35ba7e20b27f288d59dea924d84d8702e2f1b
Mehmet Aziz Algüllü <maacode@hotmail.com>
2021-09-19 12:59:39 +0300

    Unnecessary div element is deleted

--- homework-1/src/components/ListNotes/index.js
+++ homework-1/src/components/ListNotes/index.js
@@ -4,14 +4,12 @@ import { Box, Grid, Text } from '@chakra-ui/react'
 
 function ListNotes() {
     return (
-        <div>
-            <Grid templateColumns="repeat(3, 1fr)" gap={6} marginTop="2">
-                <Box bg="pink" w="100%" h="100px" overflow="auto" textAlign="center" fontSize="13px">
-                    <Text fontSize="18px" bg="red.300">Note 1</Text>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum quidem quia obcaecati veritatis, vel maiores.
-                </Box>
-            </Grid>
-        </div>
+        <Grid templateColumns="repeat(3, 1fr)" gap={6} marginTop="2">
+            <Box bg="pink" w="100%" h="100px" overflow="auto" textAlign="center" fontSize="13px">
+                <Text fontSize="18px" bg="red.300">Note 1</Text>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum quidem quia obcaecati veritatis, vel maiores.
+            </Box>
+        </Grid>
     )
 }
 
