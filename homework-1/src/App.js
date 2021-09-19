3fc50d58fe9ac5b9d0965cdd23dbc5e149c52c50
Mehmet Aziz Algüllü <maacode@hotmail.com>
2021-09-19 12:57:56 +0300

    Added ListNotes Component and integrated in App.js. ChakraUI's Grid System is used here.

--- homework-1/src/App.js
+++ homework-1/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 // import { Flex } from '@chakra-ui/react'
 import SearchInput from './components/SearchInput/';
 import AddNotes from './components/AddNotes/';
+import ListNotes from './components/ListNotes'
 
 function App() {
   return (
     <div className="container">
       <SearchInput />
       <AddNotes />
+      <ListNotes />
     </div>
     // <Flex align="center" justify="center" backgroundColor="#EAEAEA">
     //   <SearchInput />
