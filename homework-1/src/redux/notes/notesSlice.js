9c9a7931fb156d53e75b017c7fc2a11703877d29
Mehmet Aziz Algüllü <maacode@hotmail.com>
2021-09-19 13:31:08 +0300

    Added initial state(4 notes) and added color prop for every notes.

--- homework-1/src/redux/notes/notesSlice.js
+++ homework-1/src/redux/notes/notesSlice.js
@@ -3,7 +3,28 @@ import { createSlice } from '@reduxjs/toolkit'
 export const notesSlice = createSlice({
     name: 'notes',
     initialState: {
-        items: []
+        items: [
+            {
+                id: 1,
+                title: 'Fizik tekrarı',
+                color: 'pink',
+            },
+            {
+                id: 2,
+                title: '5 gün sonra kız arkadaşımın doğum günü. O güne özel hazırlık yapmalıyım. Dışarı çıkıp eğlenmeliyiz.',
+                color: 'purple',
+            },
+            {
+                id: 3,
+                title: 'En az 3 saat kod yaz.',
+                color: 'yellow',
+            },
+            {
+                id: 3,
+                title: 'Matematik tekrarı',
+                color: 'green',
+            },
+        ]
     },
     reducers: {
         addNote: {
@@ -14,3 +35,6 @@ export const notesSlice = createSlice({
         }
     }
 })
+
+export const { addNote } = notesSlice.actions;
+export default notesSlice.reducer;
