80c430c95d8497b5466e5608f8eb735b7c1c2989
Mehmet Aziz Algüllü <maacode@hotmail.com>
2021-09-23 15:40:58 +0300

    Datas from api is showing on Home Component. When the error is occured it will return Error Component and if the loading state is continued it will return Loading Component.

--- breaking-bad-app/src/pages/Home/index.js
+++ breaking-bad-app/src/pages/Home/index.js
@@ -1,20 +1,60 @@
 import { useEffect } from 'react'
 
+import Error from '../../components/Error'
+import Loading from '../../components/Loading'
+
 import { useSelector, useDispatch } from 'react-redux'
 import { fetchCharacters } from '../../redux/characters/charactersSlice'
 
 function Home() {
-    const data = useSelector(state => state.characters);
+    const characters = useSelector(state => state.characters.items);
+    const isLoading = useSelector(state => state.characters.isLoading);
+    const error = useSelector(state => state.characters.error);
     const dispatch = useDispatch();
 
     useEffect(() => {
         dispatch(fetchCharacters());
     }, [dispatch])
 
-    console.log(data);
+    if (isLoading) {
+        return <Loading />
+    }
+
+    if (error) {
+        return <Error error={error} />
+    }
+
+    console.log(characters);
     return (
         <div>
-            Home
+            <h1>Characters</h1>
+
+            {
+                characters.map((character) => (
+                    <div key={character.char_id} style={{
+                        display: 'inline-block',
+                        margin: 5,
+                        border: '15px solid #d9d9d9'
+                    }}>
+                        <img
+                            src={character.img}
+                            alt={character.name}
+                            width="200px"
+                            height="280px"
+                            style={{ objectFit: 'cover' }}
+                        />
+                        <h3 style={{
+                            textAlign: 'center',
+                            margin: '0',
+                            background: '#545863',
+                            color: '#fff'
+                        }}>
+                            {character.name}
+                        </h3>
+                    </div>
+                )
+                )
+            }
         </div>
     )
 }
--- breaking-bad-app/src/redux/characters/charactersSlice.js
+++ breaking-bad-app/src/redux/characters/charactersSlice.js
@@ -2,10 +2,14 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 import axios from 'axios'
 
+const char_limit = 10;
+
 export const fetchCharacters = createAsyncThunk(
     'characters/getCharacters',
     async () => {
-        const res = await axios(`${process.env.REACT_APP_API_BASE_ENDPOINT}/characters?limit=10`);
+        const res = await axios(`
+        ${process.env.REACT_APP_API_BASE_ENDPOINT}/characters?limit=${char_limit}`);
+
         return res.data;
     }
 );
@@ -14,11 +18,22 @@ export const charactersSlice = createSlice({
     name: 'characters',
     initialState: {
         items: [],
+        isLoading: false,
+        error: null,
     },
     reducers: {},
     extraReducers: {
+        [fetchCharacters.pending]: (state, action) => {
+            state.isLoading = true;
+        },
         [fetchCharacters.fulfilled]: (state, action) => {
-            console.log(action.payload);
+            console.log("action.load =>", action.load);
+            state.items = action.payload;
+            state.isLoading = false;
+        },
+        [fetchCharacters.rejected]: (state, action) => {
+            state.isLoading = false;
+            state.error = action.error;
         }
     }
 });
