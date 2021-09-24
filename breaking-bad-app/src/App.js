647e4a66875ad5585da6f4cd13dcad6d54a8db76
Mehmet Aziz Algüllü <maacode@hotmail.com>
2021-09-24 11:42:34 +0300

    Added Detail Component and its path in App.js

--- breaking-bad-app/src/App.js
+++ breaking-bad-app/src/App.js
@@ -2,18 +2,20 @@ import './App.css';
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
 
 import Home from './pages/Home'
+import Detail from './pages/Detail'
 
 function App() {
   return (
     <Router>
       <div>
         <Switch>
-          <Route path="/" component={Home} />
+          <Route exact path="/" component={Home} />
+          {/* exact => sadece şu path olduğunda bunu göster. */}
+          <Route path="/char/:char_id" component={Detail} />
         </Switch>
       </div>
     </Router>
