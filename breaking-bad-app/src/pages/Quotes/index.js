591de19ee2b239c5baf0b309414827918b722521
Mehmet Aziz Algüllü <maacode@hotmail.com>
2021-11-02 19:38:57 +0300

    Added quotes component for showing popular quotes.

---
+++ breaking-bad-app/src/pages/Quotes/index.js
@@ -0,0 +1,48 @@
+import { useEffect } from 'react'
+
+import { useSelector, useDispatch } from 'react-redux'
+
+import Error from '../../components/Error'
+import Loading from '../../components/Loading'
+
+import Item from './item'
+
+import {
+    fetchAllQuotes,
+    quotesSelector,
+    statusSelector,
+    errorSelector
+} from '../../redux/quotes/quotesSlice'
+
+function Quotes() {
+    const data = useSelector(quotesSelector);
+    const status = useSelector(statusSelector);
+    const error = useSelector(errorSelector);
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        if (status === 'idle') {
+            dispatch(fetchAllQuotes());
+        }
+    }, [dispatch]);
+
+    if (error) return <Error error={error} />
+
+
+    console.log(data);
+
+    return (
+        <div style={{ padding: '15px' }}>
+            <h1>Quotes</h1>
+
+            {status === 'loading' && <Loading />}
+
+            {status === 'succeeded' && data.map((item, key) => <Item key={key} item={item} />)}
+
+            {status === 'succeeded' && <div className="quotes_info">{data.length} quotes</div>}
+        </div>
+    )
+}
+
+export default Quotes
