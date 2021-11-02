7102ecf99d686ac12bd70812c70fd6550b1fa9ab
Mehmet Aziz Algüllü <maacode@hotmail.com>
2021-11-02 19:39:21 +0300

    Quotes Component integrated in App.js

--- breaking-bad-app/src/App.js
+++ breaking-bad-app/src/App.js
@@ -2,20 +2,34 @@ import './App.css';
 import {
   BrowserRouter as Router,
   Switch,
-  Route
+  Route,
+  Link
 } from "react-router-dom";
 
+//pages
 import Home from './pages/Home'
 import Detail from './pages/Detail'
+import Quotes from './pages/Quotes'
 
 function App() {
   return (
     <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Characters</Link>
+          </li>
+          <li>
+            <Link to="/quotes">Quotes</Link>
+          </li>
+        </ul>
+      </nav>
       <div>
         <Switch>
           <Route exact path="/" component={Home} />
           {/* exact => sadece şu path olduğunda bunu göster. */}
           <Route path="/char/:char_id" component={Detail} />
+          <Route path="/quotes" component={Quotes} />
         </Switch>
       </div>
     </Router>
