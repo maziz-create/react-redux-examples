70bba54910eb4ba545b073eff9eb149cd204795a
Mehmet Aziz Algüllü <maacode@hotmail.com>
2021-11-02 19:38:28 +0300

    Added quotesSlice and integrated in store.

--- breaking-bad-app/src/redux/store.js
+++ breaking-bad-app/src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 
 import charactersSlice from './characters/charactersSlice'
+import quotesSlice from './quotes/quotesSlice';
 
 export const store = configureStore({
     reducer: {
         characters: charactersSlice,
+        quotes: quotesSlice,
     },
 }); 
